19e2663412db453c812d097ef36de7a5277e10eb
Paul Harawa <paulharawa@yahoo.com>
2025-03-24 16:52:15 +0200

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 
 import "./App.css";
 import Sidebar from "./components/Navbar";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
       <Sidebar />
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,24 +26,36 @@ const Sidebar = () => {
           isOpen ? "w-64" : "w-0"
         } transition-width duration-300 bg-teal-800 text-white h-screen overflow-hidden`}
       >
-        <div className="p-4 flex flex-col ">
+        <div className="p-4 flex flex-col h-screen">
           <div className="flex flex-row space-x-2  pb-8">
             <FaCamera className="size-8 text-teal-300" />
             <h2 className="text-lg font-tertiarySecond pt-1">Wonderland</h2>
           </div>
 
-          <ul>
-            <li className="py-2 hover:bg-gray-700 cursor-pointer">Home</li>
-            <li className="py-2 hover:bg-gray-700 cursor-pointer">Gallery</li>
-            <li className="py-2 hover:bg-gray-700 cursor-pointer">Book Us</li>
-            <li className="py-2 hover:bg-gray-700 cursor-pointer">Contacts</li>
+          <ul className="pb-10 space-y-2">
+            <li className="py-2 p-2 rounded-lg hover:bg-teal-600  cursor-pointer">
+              Home
+            </li>
+            <li className="py-2 p-2 rounded-lg hover:bg-teal-600  cursor-pointer">
+              Gallery
+            </li>
+            <li className="py-2 p-2 rounded-lg hover:bg-teal-600  cursor-pointer">
+              Book Us
+            </li>
+            <li className="py-2 p-2 rounded-lg hover:bg-teal-600  cursor-pointer">
+              Contact Us
+            </li>
           </ul>
 
-          <div className="flex flex-row space-x-2 pb-8 pt-8 bottom-0">
-            <FaFacebook className="size-8 text-teal-300 hover:text-teal-200 cursor-pointer" />
-            <FaInstagram className="size-8 text-teal-300 hover:text-teal-200 cursor-pointer" />
-            <FaYoutube className="size-8 text-teal-300 hover:text-teal-200 cursor-pointer" />
-            <FaTwitter className="size-8 text-teal-300 hover:text-teal-200 cursor-pointer" />
+          <div className="flex flex-row flex-grow ">
+            <div className=" self-end w-full">
+              <div className="flex flex-row space-x-2 p-2 justify-evenly  ">
+                <FaFacebook className="size-7 text-teal-300 hover:text-teal-200 cursor-pointer" />
+                <FaInstagram className="size-7 text-teal-300 hover:text-teal-200 cursor-pointer" />
+                <FaYoutube className="size-7 text-teal-300 hover:text-teal-200 cursor-pointer" />
+                <FaTwitter className="size-7 text-teal-300 hover:text-teal-200 cursor-pointer" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
