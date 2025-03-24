5302ae85574e60a334a1c3f210a98262c9751d18
Paul Harawa <paulharawa@yahoo.com>
2025-03-24 21:23:50 +0200

    new update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,17 +9,30 @@ function Hero() {
     <div className="h-screen bg-[url(./assets/hero.jpg)] bg-cover bg-center bg-fixed">
       <div className="w-full h-full bg-[rgba(0,0,0,0.75)]">
         <div className="flex flex-col text-center h-full justify-center space-y-7">
-          <h1 className="text-gray-200 text-3xl font-bold font-secondary">
+          <h1 className="text-gray-200 text-3xl font-bold font-secondary px-3">
             Welcome To Wonderland Studios
           </h1>
-          <p className="text-yellow-300 text-md font-semibold font-mono">
+          <p className="text-yellow-300 text-md font-semibold font-mono px-3">
             where creativity meets expectations!
           </p>
           <div className="pt-10">
             <TestimonialSlider />
           </div>
+
+          <div className="pt-10">
+            <div>
+              <a href="#" className="bg-teal-700 text-gray-200 font-semibold shadow-lg px-4 py-3 rounded-full hover:bg-teal-600 cursor-pointer" >
+                Book A Session
+              </a>
+            </div>
+
+          </div>
+
+
+
         </div>
       </div>
+      
     </div>
   );
 }
